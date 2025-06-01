aeca25e3863080cbdac47cadfa67780da8fd29e4
galifra <gabrielukas@yahoo.com>
2025-05-31 21:13:09 -0500

    Update PracticeCreateaCalculator.js

--- PracticeCreateaCalculator.js
+++ PracticeCreateaCalculator.js
@@ -1,9 +1,1 @@
-Function getAbsoluteValue(number) {
-    if (number < 0){
-        return -number; //Flip it to positive
-    }
-    Return number; // Already positive
-}
-Function CalculatePower(Base, exponent) {
-    return base ** exponent;
-}
+

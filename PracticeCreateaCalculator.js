7e92055aeee4c5f695c03674f9fdf458d31b7d12
galifra <gabrielukas@yahoo.com>
2025-05-31 21:09:47 -0500

    Add files via upload

---
+++ PracticeCreateaCalculator.js
@@ -0,0 +1,9 @@
+Function getAbsoluteValue(number) {
+    if (number < 0){
+        return -number; //Flip it to positive
+    }
+    Return number; // Already positive
+}
+Function CalculatePower(Base, exponent) {
+    return base ** exponent;
+}
